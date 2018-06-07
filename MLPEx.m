1da3e8c955124ef984415098bb3ef622ac2bb816
kareemchan <kareem.chanfreau@gmail.com>
2018-06-06 21:34:47 -0500

    Update MLPEx.m

--- MLPEx.m
+++ MLPEx.m
@@ -1,5 +1,6 @@
 function MLPEx
-	[P,T]=obtenerDataset;%dataset
+	P=obtenerDataset;%inputs
+	T=obtenerDataset;%targets
 	[vcn,vtf]=obtenerArquitectura;%arquitectura
 	[w,b]=initWaB;%inicialización de pesos y bias
 	[alpha,maxepoch,minEtrain,valepoch,numval,dEnt]=obtenerDatos;%datos de validación
@@ -8,9 +9,16 @@ function MLPEx
 	graficar(w,b,vtf,P,T,Eit)
 end
 
-function [P,T]=obtenerDataset
-	P=-2:.2:2;
-	T=1+sin((pi/4)*P);
+function dataset=obtenerDataset
+    [Prot1, a1] = uigetfile('*.txt', 'Seleccione un archivo'); %abrimos ventana para seleccionar archivo
+    if isequal(Prot1, 0)
+        return
+    else
+        archivo=fullfile(a1, Prot1); % variable archivo contiene el nombre de la direccion
+    end
+    Lec_archivo = textread(archivo, '%s', 'delimiter', '\n','whitespace', ''); %Leeemos el contenido del archivo y se guarda como un cell array
+    A = cell2mat(Lec_archivo); %Convertimos el cell array a una char matriz
+    dataset = str2double(A); %Convertimos la char matrix a una double matrix, matriz p
 end
 function [vcn,vtf]=obtenerArquitectura
 	vcn=[1 2 1];
