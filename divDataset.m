99612a66148b1a0b48ab22c4b9273347f0be6b34
gogonso <edycortesperez@gmail.com>
2018-06-10 12:47:22 -0500

    actualizacion de la funcion divDataset

--- divDataset.m
+++ divDataset.m
@@ -1,10 +1,19 @@
-function [mEnt,mVal,mPru]=divDataset(P,dEnt)
+function [mEnt,mVal,mPru,tEnt,tVal,tPru]=divDataset(P,T,dEnt)%si los datos son mas 1000 el criterio es 80 10 10 si no es 70 15 15
 	tam=length(P);
-	tam1=(tam*dEnt(1))/100;%saca el tama�o de la matriz de entrenamiento
-	tam1=round(tam1);%se redondea el valor de la division por si da decimal
-	tam2=(tam-tam1)/2;%saca el tama�o de las matrices de validacion y prueba
-	mEnt=P(1:tam1);%asina los valores a la matriz de entrenamiento
-	mVal=P((tam1+1):(tam1+tam2));%asigna valores a la matriz de validacion
-	mPru=P((tam-tam2+1):tam);%asigna valroes a la matriz de prueba
+	tam2=(tam*dEnt(2))/100;%saca el tamaño de la matriz de validacion y prueba
+	tam2=round(tam2);%se redondea el valor de la division por si da decimal
+	tam1=(tam-(tam2*2));%saca el tamaño de la matriz de entrenamiento
+    V_aux(randperm(tam))=1:tam; %numeros aleatorios de 1 hasta el tamaño del vector P
+   % V_aux2(randperm(tam))=1:tam;
+    for i=1:tam %ciclo que recorrera el tamaño de los vectores P y T
+        aux=V_aux(i);%variable auxiliar que toma el valor que asigno V_aux
+        V_aux(i)=P(aux); %asignacion de valores del vector P a V_aux
+        V_aux2(i)=T(aux); %asignacion de valores del vector T a V_aux2
+    end
+	mEnt=V_aux(1:tam1)%asina los valores a la matriz de entrenamiento
+	mVal=V_aux((tam1+1):(tam1+tam2))%asigna valores a la matriz de validacion
+	mPru=V_aux((tam-tam2+1):tam)%asigna valroes a la matriz de prueba
+    tEnt=V_aux2(1:tam1);%asina los valores a la matriz de entrenamiento
+	tVal=V_aux2((tam1+1):(tam1+tam2));%asigna valores a la matriz de validacion
+	tPru=V_aux2((tam-tam2+1):tam);%asigna valroes a la matriz de prueba
 end
-
