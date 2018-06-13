4b538bb84f9f158821ddf1cb1a111c5510701aa8
gogonso <edycortesperez@gmail.com>
2018-06-13 05:54:20 -0500

    actualizacion inicializardatos

--- inicializardatos.m
+++ inicializardatos.m
@@ -1,6 +1,10 @@
 function [w,b]=inicializardatos(vcn,P,T)
+    w={ };
+	b={ };
     tamEnt=size(P);
     tamTar=size(T);
-    w= -1 + (1 + 1) * rand (vcn(2),tamEnt(1));
-    b= -1 + (1 + 1) * rand (vcn(2),tamTar(1));
+    w{1}= -1 + (1 + 1) * rand (vcn(2),tamEnt(1));
+    b{1}= -1 + (1 + 1) * rand (vcn(2),tamTar(1));
+    w{2}= -1 + (1 + 1) * rand (vcn(3),tamEnt(1));
+    b{2}= -1 + (1 + 1) * rand (vcn(3),tamTar(1));
 end
